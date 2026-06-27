861862156aba32ecdc5924af7848f2ab717a60f0
Ersin Aydıngil <erso@192.168.1.17>
2026-06-28 01:09:40 +0300

    fix: add trust proxy

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ require("dotenv").config();
 const app  = express();
 const PORT = process.env.PORT || 3000;
 
+app.set("trust proxy", 1);
+
 app.use(helmet());
 app.use(express.json({ limit: "32kb" }));
 
